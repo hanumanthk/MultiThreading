a3aabd71ed316a7694ee94d26c678a27b4b1ca72
iOS Training <iostraining@ganeshs-macbook-pro.local>
2019-01-04 13:22:18 +0530

    commit

--- MultiThreadingPractice/ViewController.swift
+++ MultiThreadingPractice/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        testMultithreadingOperations()
+        
     }
-
-
+    
+    func testMultithreadingOperations() {
+        let serialQueue = DispatchQueue(label: "My Serial Queue")
+        
+        let myMultithreadedQueue = DispatchQueue(label: "MultiThreadedQueue", qos: .default, attributes: .concurrent, autoreleaseFrequency: .inherit, target: nil)
+        
+        
+        serialQueue.async {
+            
+            for index in 1...10000 {
+                print("First Task index -- \(index)")
+            }
+        }
+        
+        serialQueue.async {
+            for index in 10001...15000 {
+                print("Second Task index -- \(index)")
+            }
+        }
+        
+        print("Hello Ganesh !!!")
+        print("Hello Pritam !!!")
+        print("Hello Evrybody !!!!")
+    }
+    
+    
+    
+    
+    
 }
-
